ad8c3b53cc7869d04c783d237d15f981266f9b64
mellow <mvllow@icloud.com>
2020-07-09 20:06:20 -0500

    fix shared word cases, eg. _text & _textMuted

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,12 +1,14 @@
 export const replaceJsonValues = (obj, searchFor, replaceWith) => {
   let result = {}
-  let regex = new RegExp(`\"${searchFor}\"`, 'g')
+  let regex = new RegExp(searchFor, 'g')
 
   Object.keys(obj).forEach((key) => {
     if (typeof obj[key] === 'object') {
       replaceJsonValues(obj[key], searchFor, replaceWith)
     } else if (typeof obj[key] === 'string') {
-      obj[key] = obj[key].replace(regex, replaceWith)
+      if (obj[key].length == searchFor.length) {
+        obj[key] = obj[key].replace(regex, replaceWith)
+      }
     }
 
     result[key] = obj[key]
