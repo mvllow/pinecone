2a65737a64e00495c319d264d3cc9262977b87a9
mvllow <mvllow@icloud.com>
2021-01-26 21:28:39 -0600

    feat: watch both config and theme file

--- source/commands/watch.ts
+++ source/commands/watch.ts
@@ -1,12 +1,22 @@
-import fs from 'fs'
 import path from 'path'
+import nodeWatch from 'node-watch'
+import { log } from '../utils/pretty-log'
+import { getConfig } from '../utils/get-config'
 import pinecone from '..'
 
 export const watch = async () => {
-	fs.watch(path.join(process.cwd(), 'pinecone.config.js'), async () => {
-		await pinecone().then(() => {
-			console.log('👀 Waiting for changes...')
-			console.log()
-		})
+	let config = getConfig()
+	let themePath = path.join(process.cwd(), config.themeFile)
+	let configPath = path.join(process.cwd(), 'pinecone.config.js')
+
+	nodeWatch([themePath, configPath], async () => {
+		await pinecone()
+			.then(() => {
+				console.log('👀 Waiting for changes...')
+				console.log()
+			})
+			.catch((error) => {
+				log.error(error)
+			})
 	})
 }
